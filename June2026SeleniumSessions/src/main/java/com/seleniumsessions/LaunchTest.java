3108251ce1c5234bafc735fe7c73f9e3e937ad75
Cheeras <shankarcheerala@gmail.com>
2026-06-20 13:44:20 +0530

    Selenium4 WebDriver Architecture and Detailed explanation

---
+++ June2026SeleniumSessions/src/main/java/com/seleniumsessions/LaunchTest.java
@@ -0,0 +1,16 @@
+package com.seleniumsessions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchTest {
+
+	public static void main(String[] args) {
+		
+		//broswer version v149
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shankar\\Documents\\SeleniumDrivers\\chromedriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+	}
+
+}
