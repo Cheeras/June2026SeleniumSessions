2785025dd8a4cac1411d6003921202347a7c6829
Cheeras <shankarcheerala@gmail.com>
2026-06-20 20:59:58 +0530

    Quit vs close and Navigation url

--- June2026SeleniumSessions/src/main/java/com/seleniumsessions/LaunchTest.java
+++ June2026SeleniumSessions/src/main/java/com/seleniumsessions/LaunchTest.java
@@ -1,7 +1,7 @@
 package com.seleniumsessions;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchTest {
 
@@ -9,8 +9,10 @@ public class LaunchTest {
 		
 		//broswer version v149
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shankar\\Documents\\SeleniumDrivers\\chromedriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		
+		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();//345
+		driver = new ChromeDriver();//567
+		driver.get("https://www.google.com/");//567
+		driver.quit();//567 it will close only last driver session i.e 567
 	}
-
 }
---
+++ June2026SeleniumSessions/src/main/java/com/seleniumsessions/QuitVsClose.java
@@ -0,0 +1,33 @@
+
+package com.seleniumsessions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class QuitVsClose {
+
+	public static void main(String[] args) {
+
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+	
+		driver.close();//after execution of this statement driver will become null
+		
+		/*
+		 * i want to fix this issue again launch the driver again
+		 * driver = new ChromeDriver();
+		 * */
+		//driver = new ChromeDriver();
+		System.out.println(driver.getTitle());
+		//org.openqa.selenium.NoSuchSessionException
+		//Session ID is null. Using WebDriver after calling quit()?
+		
+	}
+
+}
